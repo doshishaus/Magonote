f35cee5a1f735d2e4fd56482717a0a1b366bcc8a
{doshishaus} <cgeh0721@mail3.doshisha.ac.jp>
2024-06-16 13:51:46 +0900

    アイコン、こらしょ、背景

--- www/index.php
+++ www/index.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Magonote</title>
+    <link rel="stylesheet" href="./style/style.css">
 </head>
+
 <body>
-    <h1>hello world</h1>
+    <header>
+
+        <button class="icon"><img src="./img/icon/setting.svg" alt="設定" class="icon-img"></button>
+    </header>
+    <main>
+        <div class="magorun">
+            <div class="base">
+                <h3>ベースマゴルン</h3>
+            </div>
+            <div class="active">
+                <h3>アクティブマゴルン</h3>
+            </div>
+        </div>
+        <div class="room">
+            <img src="./img/corasho.png" alt="" class="corasho"s>
+        </div>
+
+    </main>
+    <footer>
+        <div class="icon-row">
+            <button class="icon"><img src="./img/icon/fork.svg" alt="" class="icon-img"></button>
+            <button class="icon"><img src="./img/icon/fork.svg" alt="" class="icon-img"></button>
+            <button class="icon"><img src="./img/icon/fork.svg" alt="" class="icon-img"></button>
+            <button class="icon"><img src="./img/icon/fork.svg" alt="" class="icon-img"></button>
+        </div>
+    </footer>
 </body>
+
 </html>
